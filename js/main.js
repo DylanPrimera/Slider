30e4454af168f4fda791ddb0f24410e06c7c3303
DylanPrimera <dylanprimera98@gmail.com>
2019-11-19 16:15:28 -0500

    final

---
+++ js/main.js
@@ -0,0 +1,65 @@
+$(document).ready(function(){
+	var imgItems = $('.slider li').length; // Numero de Slides
+	var imgPos = 1;
+
+	// Agregando paginacion --
+	for(i = 1; i <= imgItems; i++){
+		$('.pagination').append('<li><span class="fa fa-circle"></span></li>');
+	} 
+	//------------------------
+
+	$('.slider li').hide(); // Ocultanos todos los slides
+	$('.slider li:first').show(); // Mostramos el primer slider
+	$('.pagination li:first').css({'color': '#CD6E2E'}); // Damos estilos al primer item de la paginacion
+
+	// Ejecutamos todas las funciones
+	$('.pagination li').click(pagination);
+	$('.right span').click(nextSlider);
+	$('.left span').click(prevSlider);
+
+
+	//Intervalo de cambio
+	setInterval(function(){
+		nextSlider();
+	}, 3000);
+
+	// FUNCIONES =========================================================
+
+	function pagination(){
+		var PagiPos = $(this).index() + 1; // Posicion de la paginacion seleccionada
+
+		$('.slider li').hide(); // Ocultamos todos los slides
+		$('.slider li:nth-child('+ PagiPos +')').fadeIn(); // Mostramos el Slide seleccionado
+
+		// Dandole estilos a la paginacion seleccionada
+		$('.pagination li').css({'color': '#858585'});
+		$(this).css({'color': '#CD6E2E'});
+
+		imgPos = PagiPos;
+
+	}
+
+	function nextSlider(){
+		if( imgPos >= imgItems){imgPos = 1;} 
+		else {imgPos++;}
+
+		$('.pagination li').css({'color': '#858585'});
+		$('.pagination li:nth-child(' + imgPos +')').css({'color': '#CD6E2E'});
+
+		$('.slider li').hide(); // Ocultamos todos los slides
+		$('.slider li:nth-child('+ imgPos +')').fadeIn(); // Mostramos el Slide seleccionado
+
+	}
+
+	function prevSlider(){
+		if( imgPos <= 1){imgPos = imgItems;} 
+		else {imgPos--;}
+
+		$('.pagination li').css({'color': '#858585'});
+		$('.pagination li:nth-child(' + imgPos +')').css({'color': '#CD6E2E'});
+
+		$('.slider li').hide(); // Ocultamos todos los slides
+		$('.slider li:nth-child('+ imgPos +')').fadeIn(); // Mostramos el Slide seleccionado
+	}
+
+});
