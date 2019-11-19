e782b9c70d33c1d9fb2d9752a0dd32a8815524c5
DylanPrimera <dylanprimera98@gmail.com>
2019-11-19 16:34:35 -0500

    v2

--- js/main.js
+++ js/main.js
@@ -1,37 +1,37 @@
 $(document).ready(function(){
-	var imgItems = $('.slider li').length; // Numero de Slides
+	var imgItems = $('.slider li').length; 
 	var imgPos = 1;
 
-	// Agregando paginacion --
+	
 	for(i = 1; i <= imgItems; i++){
 		$('.pagination').append('<li><span class="fa fa-circle"></span></li>');
 	} 
-	//------------------------
+	
 
-	$('.slider li').hide(); // Ocultanos todos los slides
-	$('.slider li:first').show(); // Mostramos el primer slider
-	$('.pagination li:first').css({'color': '#CD6E2E'}); // Damos estilos al primer item de la paginacion
+	$('.slider li').hide();
+	$('.slider li:first').show(); 
+	$('.pagination li:first').css({'color': '#CD6E2E'}); 
 
-	// Ejecutamos todas las funciones
+	
 	$('.pagination li').click(pagination);
 	$('.right span').click(nextSlider);
 	$('.left span').click(prevSlider);
 
 
-	//Intervalo de cambio
+	
 	setInterval(function(){
 		nextSlider();
 	}, 3000);
 
-	// FUNCIONES =========================================================
+	
 
 	function pagination(){
-		var PagiPos = $(this).index() + 1; // Posicion de la paginacion seleccionada
+		var PagiPos = $(this).index() + 1; 
 
-		$('.slider li').hide(); // Ocultamos todos los slides
-		$('.slider li:nth-child('+ PagiPos +')').fadeIn(); // Mostramos el Slide seleccionado
+		$('.slider li').hide();
+		$('.slider li:nth-child('+ PagiPos +')').fadeIn();
 
-		// Dandole estilos a la paginacion seleccionada
+		
 		$('.pagination li').css({'color': '#858585'});
 		$(this).css({'color': '#CD6E2E'});
 
@@ -46,8 +46,8 @@ $(document).ready(function(){
 		$('.pagination li').css({'color': '#858585'});
 		$('.pagination li:nth-child(' + imgPos +')').css({'color': '#CD6E2E'});
 
-		$('.slider li').hide(); // Ocultamos todos los slides
-		$('.slider li:nth-child('+ imgPos +')').fadeIn(); // Mostramos el Slide seleccionado
+		$('.slider li').hide(); 
+		$('.slider li:nth-child('+ imgPos +')').fadeIn();
 
 	}
 
@@ -58,8 +58,8 @@ $(document).ready(function(){
 		$('.pagination li').css({'color': '#858585'});
 		$('.pagination li:nth-child(' + imgPos +')').css({'color': '#CD6E2E'});
 
-		$('.slider li').hide(); // Ocultamos todos los slides
-		$('.slider li:nth-child('+ imgPos +')').fadeIn(); // Mostramos el Slide seleccionado
+		$('.slider li').hide(); 
+		$('.slider li:nth-child('+ imgPos +')').fadeIn();
 	}
 
 });
